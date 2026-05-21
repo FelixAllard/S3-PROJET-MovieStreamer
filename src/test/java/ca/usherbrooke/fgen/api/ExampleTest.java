d6aff2c7891d5829a5fa28bacb2200c22327bbe4
Felix <allardfelix05@gmail.com>
2026-05-21 02:01:21 -0400

    Gitflow

---
+++ src/test/java/ca/usherbrooke/fgen/api/ExampleTest.java
@@ -0,0 +1,35 @@
+package ca.usherbrooke.fgen.api;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class ExampleTest {
+
+    @Test
+    void testEnvironmentStarted() {
+
+        System.out.println("=================================");
+        System.out.println(" QUARKUS TEST ENVIRONMENT START ");
+        System.out.println("=================================");
+
+        // Example assertions:
+        // assertEquals(expected, actual);
+        // assertTrue(condition);
+        // assertNotNull(object);
+
+        // Example REST test:
+        //
+        // given()
+        //   .when().get("/hello")
+        //   .then()
+        //      .statusCode(200);
+
+        // Example injection:
+        //
+        // @Inject
+        // MyService service;
+
+        System.out.println("Example test executed successfully.");
+    }
+}
