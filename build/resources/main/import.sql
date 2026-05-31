58f0e083d44053400938c054bde11bbe51b43153
mathieuvincent2006 <mathieu.vincent.2006@gmail.com>
2026-05-31 00:39:46 -0400

    fix: retirer fichiers generés du suivi Git

--- build/resources/main/import.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO app."user" (name, surname, email) VALUES ('Marie', 'Tremblay', 'marie.tremblay@gmail.com');
-INSERT INTO app."user" (name, surname, email) VALUES ('Jean', 'Gagnon', 'jean.gagnon@gmail.com');
-INSERT INTO app."user" (name, surname, email) VALUES ('Sophie', 'Bouchard', 'sophie.bouchard@gmail.com');
-INSERT INTO app."user" (name, surname, email) VALUES ('Lucas', 'Fortin', 'lucas.fortin@gmail.com');
-INSERT INTO app."user" (name, surname, email) VALUES ('Emma', 'Côté', 'emma.cote@gmail.com');
