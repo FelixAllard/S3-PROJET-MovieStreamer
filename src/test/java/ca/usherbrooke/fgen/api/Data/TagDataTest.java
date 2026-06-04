b8353bf946a682f2e7911de19b4d14223a2bd0d4
seeroy667 <138810937+seeroy667@users.noreply.github.com>
2026-06-03 22:59:29 -0400

    Ok fix conflits pro max plus plus

--- src/test/java/ca/usherbrooke/fgen/api/Data/TagDataTest.java
+++ src/test/java/ca/usherbrooke/fgen/api/Data/TagDataTest.java
@@ -12,9 +12,7 @@ import ca.usherbrooke.fgen.api.Data.TagData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.wildfly.common.Assert.assertTrue;
 import ca.usherbrooke.fgen.api.DAO.TagRepository;
-import ca.usherbrooke.fgen.api.Entities.Tag;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
@@ -82,23 +80,5 @@ public class TagDataTest {
         assertFalse(result);
         verify(tagRepository, times(1)).deleteMovieTagLinksByTagId(99);
         verify(tagRepository, times(1)).deleteById(99);
-    }
-
-    @Test
-    @Transactional // Vérifier si l'ajout d'un tag retourne un tag avec un ID et un nom valide
-    public void addTag_success() {
-        Tag tag = new Tag();
-        tag.name = "Action";
-        Tag result = tagData.postTag(tag);
-        assertTrue(result.id > 0); // ID verification
-        assertEquals("Action", result.name); // Name verification
-    }
-
-    @Test
-    @Transactional
-    public void addTag_nullName_shouldFail() { // Si le tag a un nom null, on retourne une erreur
-        Tag tag = new Tag();
-        tag.name = null;
-        assertThrows(Exception.class, () -> tagData.postTag(tag));
     }
 }
